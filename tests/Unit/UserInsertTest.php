9ff2add5ca2a7f9b3019556b64b63b59a855ec96
felecia <fl248@njit.edu>
2018-07-10 16:01:05 -0400

    Completed UserInsertTest

---
+++ tests/Unit/UserInsertTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
+
+class UserInsertTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $user = new User();
+        $user->name = 'Leroy Brown';
+        $user->email = 'leroybrown@gmail.com';
+        $user->password = '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm';
+        $user->remember_token = str_random(10);
+
+        $this->assertTrue($user->save());
+    }
+}
