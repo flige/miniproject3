23a9d98c5c652234208630b91f7c33402e628c16
felecia <fl248@njit.edu>
2018-07-10 15:38:32 -0400

    Completed About 200 status code. Updated About page with @endsection so test not risky

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -6,3 +6,5 @@
     <h1>About me</h1>
     <p class="lead">Hi My name is Felecia. I am currently a graduate student at NJIT. My hobbies are pilates, learning languages, and baking.</p>
 </div>
+    @endsection
+
